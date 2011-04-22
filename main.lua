cb0d94ca62dace4ee4e7d5d368769cf18f7c38e8
Seth <Seth@Alyrica.net>
2011-04-21 21:59:17 -0700

    Corrected ship mass

--- main.lua
+++ main.lua
@@ -35,7 +35,7 @@ config = {}
 --Polymorphic variable holding the current view.
 state = {}
 --Master bag that holds the default system configuration.
-local theControlBag = controlBag:new("w","a","s","d","q","e","r","1","2","EASY",800,600,"yes","*.png",200,9,"no",4,"no",100,1,100000)
+local theControlBag = controlBag:new("w","a","s","d","q","e","r","1","2","EASY",800,600,"yes","*.png",200,9,"no",4,"no",100,1,10 ^ 20)
 
 --[[
 --Initializing function that loads the LOVE2D framework.
